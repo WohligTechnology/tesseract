2ac2408adb5f2c32186c015a3d95579b68be9926
mahesh maurya <mahesh@wohlig.com>
2016-02-29 20:24:10 +0530

    sum work done

--- js/app.js
+++ js/app.js
@@ -70,7 +70,20 @@ firstapp.config(function ($stateProvider, $urlRouterProvider, $httpProvider, $lo
 
 });
 
-
+firstapp.directive('autoHeight', function($compile, $parse) {
+    return {
+        restrict: 'EA',
+        replace: false,
+        link: function($scope, element, attrs) {
+            var $element = $(element);
+            var windowHeight = $(window).height();
+            var addHeight = function() {
+                $element.css("min-height", windowHeight);
+            };
+            addHeight();
+        }
+    };
+});
 firstapp.directive('img', function ($compile, $parse) {
 	return {
 		restrict: 'E',
