042f17330dbd8d90cf335f5753a541d58f37d84f
sohan1 <shn619@gmail.com>
2016-03-23 10:17:17 +0530

    ng animate

--- js/controllers.js
+++ js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngSanitize', 'angular-flexslider'])
+angular.module('phonecatControllers', ['templateservicemod', 'navigationservice', 'ui.bootstrap', 'ngAnimate', 'ngSanitize', 'angular-flexslider'])
 
 .controller('HomeCtrl', function($scope, TemplateService, NavigationService, $timeout) {
   //Used to name the .html for particular template file
